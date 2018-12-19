be5ea7582b1d0cf498e0fd082c87ec00661f34f8
wmm1996528 <912594746@qq.com>
2018-12-19 17:24:53 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,21 @@
+"""
+-------------------------------------------------
+   File Name：     config
+   Description :
+   Author :       wang
+   date：          2018/12/19
+-------------------------------------------------
+   Change Activity:
+                   2018/12/19:
+-------------------------------------------------
+"""
+__author__ = 'wang'
+import sys
+
+sys.path.append(r'C:\Users\wang\Desktop\Tools')
+from logg import *
+
+"""
+设置logger = logClass('name') 启动日志文件
+"""
+logger = logClass(__name__)
